57c0e112675a453c7df08e80c69730c24a6ea1b1
SUNKARA NAGA SAI RAM <snsram089.mca2023@ca.nitrr.ac.in>
2025-07-08 16:33:44 +0530

    Update StickyNavbar.jsx
    nav bar logo update

--- src/components/StickyNavbar.jsx
+++ src/components/StickyNavbar.jsx
@@ -1,10 +1,14 @@
 // src/components/StickyNavbar.jsx
 import React from 'react';
-
+import logo from '../ChatGPT Image Jul 4, 2025, 12_40_21 PM.png'
 export default function StickyNavbar({ mode, setMode }) {
   return (
     <div className="sticky top-0 bg-white py-3 px-6 z-50 flex justify-between items-center shadow">
-      <h1 className="text-xl font-bold text-gray-800">TrashClash 🛡️</h1>
+{/*       <h1 className="text-xl font-bold text-gray-800">TrashClash 🛡️</h1> */}
+    <div className="flex items-center gap-2">
+    <img src="{logo}" alt="TrashClash" className="h-8 w-8 object-contain" />
+    <span className="text-xl font-bold">TrashClash</span>
+    </div>
       <div className="flex gap-2">
         <button
           onClick={() => setMode('builder')}
